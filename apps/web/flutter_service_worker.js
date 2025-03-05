35bf62ee72961f501878e0a0fb0254e3ffac3fa5
kaiqa <apiman@arcor.de>
2025-03-05 11:32:28 +0100

    file fix

--- apps/web/flutter_service_worker.js
+++ apps/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "438c40db909c9e64ebafb1f129e0bd69",
+const RESOURCES = {"flutter_bootstrap.js": "b8cb89060126a601955ab8dc415ad8e8",
 "version.json": "3dd4ac04faf5c5f596f25c56477e8d1b",
 "index.html": "4c945e6475bf0ed52c81aa88ac712e24",
 "/": "4c945e6475bf0ed52c81aa88ac712e24",
-"main.dart.js": "396d87e98f9e6a4c49cf13ebd8ebe7e6",
+"main.dart.js": "95388bf1d30baf18074d3e0c26d5db4d",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
